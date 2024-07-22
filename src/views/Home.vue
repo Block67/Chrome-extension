f869848ac0f64da011a921394524cfb47c1e7864
horus-onchain <block6703@gmail.com>
2024-07-22 09:49:58 +0100

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <h2>Welcome to your Vue.js Chrome Extension</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+};
+</script>
+
+<style scoped>
+.home {
+  text-align: center;
+}
+</style>
